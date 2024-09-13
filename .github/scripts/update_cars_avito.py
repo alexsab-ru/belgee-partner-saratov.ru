14556018eb95d4b1cb526dc3d1289e3f244254e2
Alexander Sabanov <alexsab@mail.ru>
2024-09-13 12:33:36 +0400

    Update update_cars_avito.py

--- .github/scripts/update_cars_avito.py
+++ .github/scripts/update_cars_avito.py
@@ -53,11 +53,11 @@ for car in cars_element:
     print(f"Уникальный идентификатор: {unique_id}")
     create_child_element(car, 'url', f"https://{repo_name}/cars/{unique_id}/")
     # Создаем дубликаты, но не добавляем их сразу в cars_element
-    duplicates = duplicate_car(car, 1, "в наличии")
+    duplicates = duplicate_car(car, 1, "в наличии", 9)
     all_duplicates.extend(duplicates)  # Добавляем дубликаты в отдельный список
-    duplicates = duplicate_car(car, 7)
+    duplicates = duplicate_car(car, 7, "в пути", 8)
     all_duplicates.extend(duplicates)  # Добавляем дубликаты в отдельный список
-    duplicates = duplicate_car(car, 1, "на заказ")
+    duplicates = duplicate_car(car, 1, "на заказ", 7)
     all_duplicates.extend(duplicates)  # Добавляем дубликаты в отдельный список
 
 # После окончания основного цикла добавляем все дубликаты в cars_element
